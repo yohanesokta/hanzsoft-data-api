9ab5d92685ecd85d73843b49326327622f3e9099
yohanesokta <yohanesoktayt72@gmail.com>
2023-09-04 06:44:26 -0500

    add enpoint put

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,7 +1,6 @@
 // global function routes
 const mainControl = require('../app/controllers/mainControllers')
 const midControl = require('../app/controllers/middleControllers')
-const { display } = require('../app/config/display')
 
 
 function globalRoutes(app) {
@@ -19,21 +18,20 @@ function globalRoutes(app) {
     })
 
     app.post('/api',(req,res)=>{
-        display(" '/api' on Post ")
         mainControl.add(req,res)
     })
 
     app.get("/api",(req,res)=>{
-        display(" '/api' on GET ")
         mainControl.get(req,res)
     })
 
     app.get("/api/kategori",(req,res)=>{
-        display(" '/api/kategori' on GET")
         midControl.get(req,res)
     })
+
+
+
     app.post("/api/kategori",(req,res)=>{
-        display(" '/api/kategori' on Post")
         midControl.add(req,res)
     })
 
